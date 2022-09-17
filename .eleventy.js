4379981903c976b3bae4d2a1a50bfa1487ef366d
Raphael Höser <raphael@hoeser.info>
2022-09-17 17:28:09 +0200

    Test rollup plugin serverless
    Signed-off-by: Raphael Höser <raphael@hoeser.info>

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,19 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
-const { readdirSync } = require("fs");
-
+const rollupPlugin = require("eleventy-plugin-rollup");
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addShortcode("cwd", () => process.cwd());
-  eleventyConfig.addShortcode("dir", (path) => readdirSync(path).join("<br>"));
-
+  eleventyConfig.addPlugin(rollupPlugin, {
+    rollupOptions: {
+      output: {
+        format: "es",
+        dir: "_site/js",
+        sourcemap: process.env.NETLIFY !== "true",
+      },
+    },
+  });
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "possum", // The serverless function name from your permalink object
     functionsDir: "./netlify/functions/",
   });
-
   // Return your Object options:
   return {
     markdownTemplateEngine: "njk",
--- src/index.md
+++ src/index.md
@@ -6,6 +6,4 @@ permalink:
 
 Test
 
-{% cwd %}
-
-{% dir "." %}
+{% rollup "js/test.js" %}
